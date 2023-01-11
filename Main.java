5c23a5efbafd99b95e8b3dedd68ae8c6f974b8a0
Conner <99757387+hallconner@users.noreply.github.com>
2023-01-11 15:24:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    // Public: Main Method
+    // void: return type
+
+    public static void main(String args[]) {
+        int score = 0;
+        float partialScore = 2.56f;
+        System.out.println("Hello my name is ");
+        System.out.println("The score is: " + score);
+        System.out.println("The partial score is: " + partialScore);
+    }
+}
